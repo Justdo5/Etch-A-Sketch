7eb498eaa738f71b8f61d0b793a20fc44909281d
Justdo5 <188883725+Justdo5@users.noreply.github.com>
2025-10-14 22:06:04 +0100

    Add changeColor function to createSquares
    When user moves over squares, it will create a pixilated trail of
    randomly generated colors.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 const containerForSquares = document.querySelector(".container")
 
-
-
 function createSquares(num) {
     for(let i = 0; i < num * num; i++) {
         const square = document.createElement("div")
@@ -9,7 +7,25 @@ function createSquares(num) {
         containerForSquares.appendChild(square)
         square.style.height = (600/num) + "px";
         square.style.width = (600/num) + "px";
+        square.addEventListener("mouseenter", changeColor)
+        square.addEventListener("mouseleave", defaultColor)
+
+        function changeColor() {
+            let random1 = Math.random() * 256 + 1
+            let random2 = Math.random() * 256 + 1
+            let random3 = Math.random() * 256 + 1
+          square.style.backgroundColor =  `rgb(${random1},${random2},${random3})`
+        }
+
+        function defaultColor() {
+            square.style.backgroundColor =  "lightblue"
+        }
     }
 }
 
-createSquares(16)
+createSquares(19)
+
+
+
+
+
