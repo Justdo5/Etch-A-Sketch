989be510726019497b99fc16512bd0934ca42d2e
Justdo5 <188883725+Justdo5@users.noreply.github.com>
2025-10-14 17:04:09 +0100

    Centralise container by adding html type selector

--- javascript.js
+++ javascript.js
@@ -2,4 +2,19 @@ const containerForSquares = document.querySelector(".container")
 
 
 
+// function adjustWidth (num) {
+    
+//     const square = document.createElement("div")
+//     square.className = "addSquare"
+//     square.style.backgroundColor = "blue";
+    
+//     square.style.width = (600/num) + "px";
+
+//     square.style.height = (600/num) + "px";
+//     containerForSquares.appendChild(square)
+
+//     return square.style.width
+// }
+
+// console.log(adjustWidth(9))
 
