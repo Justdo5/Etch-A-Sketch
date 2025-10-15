879a7a34786d6163870fc14417433ffbeec26d4e
Justdo5 <188883725+Justdo5@users.noreply.github.com>
2025-10-15 20:58:47 +0100

    Add buttonContainer class to store buttons

--- javascript.js
+++ javascript.js
@@ -1,37 +1,47 @@
 const containerForSquares = document.querySelector(".container")
 
+const buttonContainer = document.createElement("div")
+buttonContainer.className= "buttonContainer"
+document.body.appendChild(buttonContainer)
+
+
+
+
 function createSquares(num) {
-    num = parseInt(prompt("Number of squares per side"))
-    while(num >= 100){
+    function enterNumber(){
         num = parseInt(prompt("Number of squares per side"))
+        while(num >= 100){
+            num = parseInt(prompt("Number of squares per side"))
+        }
     }
-    console.log(num)
+    enterNumber()
+
     for(let i = 0; i < num * num; i++) {
-       
         const square = document.createElement("div")
         square.className = "addSquare"
-        containerForSquares.appendChild(square)
         square.style.height = (600/num) + "px";
         square.style.width = (600/num) + "px";
         square.addEventListener("mouseenter", changeColor)
         square.addEventListener("mouseleave", defaultColor)
-
+        containerForSquares.appendChild(square)
+        
         function changeColor() {
             let randomColor1 = Math.random() * 256 + 1
             let randomColor2 = Math.random() * 256 + 1
             let randomColor3 = Math.random() * 256 + 1
             square.style.backgroundColor =  `rgb(${randomColor1},${randomColor2},${randomColor3})`
         }
-
         function defaultColor() {
             square.style.backgroundColor =  "lightblue"
         }
     }
 }
 
-createSquares()
 
 
+   
+
+    
 
 
 
